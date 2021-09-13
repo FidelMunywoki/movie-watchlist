ff6836ca81b876dd76ae100b20468ba2d29d3786
FidelMunywoki <fidelis.munywoki@student.moringaschool.com>
2021-09-13 20:24:13 +0300

    updae

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,32 @@
-from app.config import DevConfig
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
 
-#iitializing the appication
+bootstrap = Bootstrap()
 
-app = Flask(__name__, instance_relative_config = True)
+def create_app(config_name):
 
-# setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+    app = Flask(__name__)
 
-from app import views
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Will add the views and forms
+    
+     # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # setting config
+    from .requests import configure_request
+    configure_request(app)
+
+
+    return app
+
 
 
 
---
+++ start.sh
@@ -0,0 +1,3 @@
+export MOVIE_API_KEY='42adcda2353ac2b1886b5a5993ce7f68'
+export  SECRET_KEY = 'os.urandom(32)'
+python3.9 manage.py server
